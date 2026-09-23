1aa2932db92658ddf2a5c2c1120b803d6ddee0c7
mahi <mahadeviraut.777@gmail.com>
2026-09-23 12:44:20 +0530

    Initial commit

---
+++ D.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class D {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks of Subject 1: ");
+        int m1 = sc.nextInt();
+
+        System.out.print("Enter marks of Subject 2: ");
+        int m2 = sc.nextInt();
+
+        System.out.print("Enter marks of Subject 3: ");
+        int m3 = sc.nextInt();
+
+        int total = m1 + m2 + m3;
+        double percentage = total / 3.0;
+
+        char grade;
+
+        if (percentage >= 75)
+            grade = 'A';
+        else if (percentage >= 60)
+            grade = 'B';
+        else if (percentage >= 50)
+            grade = 'C';
+        else if (percentage >= 40)
+            grade = 'D';
+        else
+            grade = 'F';
+
+        System.out.println("\nTotal Marks = " + total);
+        System.out.println("Percentage = " + percentage + "%");
+        System.out.println("Grade = " + grade);
+
+        sc.close();
+    }
+}
---
+++ README.md.txt
@@ -0,0 +1,27 @@
+# Student Grade Calculator
+
+A simple Java program to calculate total marks,
+percentage, and grade of a student.
+
+## Features
+
+- Accepts marks of three subjects
+- Calculates total marks
+- Calculates percentage
+- Displays grade
+
+## Technologies Used
+
+- Java
+- Git
+- GitHub
+
+## How to Run
+
+Compile the program:
+
+javac StudentGradeCalculator.java
+
+Run the program:
+
+java StudentGradeCalculator
